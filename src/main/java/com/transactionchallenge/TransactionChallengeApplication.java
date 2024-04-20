caa9db31d6808653c9c74745a4a8f50f5bffaf1d
Fabio Vieira <fabio.v2001@gmail.com>
2024-04-20 13:01:31 -0300

    project initial setup

---
+++ src/main/java/com/transactionchallenge/TransactionChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.transactionchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transactionchallenge/TransactionChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactionchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
